eb897176a616392c89ab967bb6fed3e53b4bd068
Jeff Williams <blueryth@gmail.com>
2014-04-28 18:45:23 -0500

    Environment based off of Globals

--- Assets/Scripts/EnvironmentMovement.cs
+++ Assets/Scripts/EnvironmentMovement.cs
@@ -6,13 +6,13 @@ public class EnvironmentMovement : MonoBehaviour
     public Transform CurrentEnvironment;
     public Transform NextEnvironment;
 
-    public float ScrollSpeed;
+    public Global globals;
 
     public void Update()
     {
         if(CurrentEnvironment != null)
-            CurrentEnvironment.transform.Translate(new Vector3(-ScrollSpeed * Time.deltaTime, 0, 0));
+            CurrentEnvironment.transform.Translate(new Vector3(-globals.ScrollSpeed * Time.deltaTime, 0, 0));
         if (CurrentEnvironment != null)
-            NextEnvironment.transform.Translate(new Vector3(-ScrollSpeed * Time.deltaTime, 0, 0));
+            NextEnvironment.transform.Translate(new Vector3(-globals.ScrollSpeed * Time.deltaTime, 0, 0));
     }
 }
