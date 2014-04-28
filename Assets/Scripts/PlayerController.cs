5239a302ce72da95abf55f43a24bc798c3c89df4
WizardOfOzzy <c.brent.osborne@gmail.com>
2014-04-28 18:23:12 -0500

    fleshing out player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,25 +3,41 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	#region Public Fields
+
+	public KeyCode LeftKey;
+	public KeyCode RightKey;
+	public KeyCode JumpKey;
+	public KeyCode PowerUpKey;
+
+	#endregion
+
+	#region Monobehavior
+
 	// Use this for initialization
 	void Start () 
 	{
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-	
+		
 	}
 
-
+	#endregion
 
 	#region Private Methods
 
+	private void Death()
+	{
+		// Deathy things
+	}
+
 	private void OnKillPlaneCollision()
 	{
-
+		// Killy things
 	}
 
 	#endregion
