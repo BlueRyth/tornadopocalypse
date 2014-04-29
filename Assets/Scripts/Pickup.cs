1fc1bb9fcd1783289e9db54eb4729565c2e2cca9
WizardOfOzzyMac <wizardofozzy@gmail.com>
2014-04-28 20:38:44 -0500

    Pickup
    default pickup files so i won’t loose my files

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour {
+
+	public Global globals;
+
+	// Use this for initialization
+	void Start () {
+		renderer.material.color = Color.red;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		Debug.Log(collider.gameObject.tag);
+		
+		if (collider.gameObject.tag == globals.tag_Player)
+		{
+			GameObject.Destroy(gameObject);
+		}
+	}
+}
