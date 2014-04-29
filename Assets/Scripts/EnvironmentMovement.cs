fd589e4e6194b60cf90823533c7d0782961d7088
Jeff Williams <blueryth@gmail.com>
2014-04-28 19:36:53 -0500

    Endless runner time, biznatch

--- Assets/Scripts/EnvironmentMovement.cs
+++ Assets/Scripts/EnvironmentMovement.cs
@@ -1,18 +1,58 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System;
+using System.Collections.Generic;
 
 public class EnvironmentMovement : MonoBehaviour 
 {
+    public Transform PreviousEnvironment;
     public Transform CurrentEnvironment;
-    //public Transform NextEnvironment;
+    public Transform NextEnvironment;
+
+    public List<Transform> EnvironmentPool;
+    public float EnvironmentWidth;
 
     public Global globals;
 
+    public void Start()
+    {
+        if (CurrentEnvironment == null)
+            throw new Exception("WHAAAAAT!?!? Punk, need current environment");
+
+        if (NextEnvironment == null)
+            RollNextEnvironment();
+
+        environmentOffset = 0;
+    }
+
     public void Update()
     {
+        environmentOffset += globals.ScrollSpeed * Time.deltaTime;
         if(CurrentEnvironment != null)
-            CurrentEnvironment.transform.Translate(new Vector3(-globals.ScrollSpeed * Time.deltaTime, 0, 0));
-        //if (CurrentEnvironment != null)
-        //    NextEnvironment.transform.Translate(new Vector3(-globals.ScrollSpeed * Time.deltaTime, 0, 0));
+            CurrentEnvironment.transform.Translate(globals.LeftTranslate);
+        if (NextEnvironment != null)
+            NextEnvironment.transform.Translate(globals.LeftTranslate);
+
+        if (environmentOffset > EnvironmentWidth)
+        {
+            environmentOffset -= EnvironmentWidth;
+            PreviousEnvironment = CurrentEnvironment;
+            CurrentEnvironment = NextEnvironment;
+            RollNextEnvironment();
+            DestroyPreviousEnvironment();
+        }
     }
+
+    public void RollNextEnvironment()
+    {
+        NextEnvironment = (Transform)Instantiate(EnvironmentPool[UnityEngine.Random.Range(0, EnvironmentPool.Count - 1)]);
+        NextEnvironment.transform.position = new Vector3(environmentOffset + EnvironmentWidth, 0, 0);
+    }
+
+    public void DestroyPreviousEnvironment()
+    {
+        Destroy(PreviousEnvironment.gameObject);
+        PreviousEnvironment = null;
+    }
+
+    private float environmentOffset;
 }
